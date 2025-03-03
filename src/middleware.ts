1f08af8a3b133191d01e2dfa747e57270cc7657b
Mathis Pollet--Dassonval <124866425+Bisounours302@users.noreply.github.com>
2025-03-03 15:31:42 +0100

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-export function middleware(req: NextRequest) {
+export function middleware(_: NextRequest) {
   return new NextResponse("<h1>Site en maintenance</h1>", {
     status: 503,
     headers: { "Content-Type": "text/html" },
